4adc2f712cc2ed4782c33ec6ba43f0e2ac519c3f
yechangxiao <yechangxiaoycx@qq.com>
2022-06-12 22:54:41 +0800

    [add]: 引入elementui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'element-ui/lib/theme-chalk/index.css'
+import { Button, Form, FormItem, Input } from 'element-ui'
+
 Vue.config.productionTip = false
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 
 new Vue({
   render: h => h(App)
